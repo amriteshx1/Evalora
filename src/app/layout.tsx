86ccbe3ce726bcbc309542fb8eb63dff4508f262
Amritesh Gautam <amriteshx18@gmail.com>
2026-03-16 19:42:48 +0530

    add main layout for the application

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { NuqsAdapter } from "nuqs/adapters/next";
+
+import { TRPCReactProvider } from "@/trpc/client";
+
+import { Toaster } from "@/components/ui/sonner";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter", 
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Wiora",
+  description: "AI-powered meeting platform where custom agents join live calls and provide post call insights.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <NuqsAdapter>
+      <TRPCReactProvider>
+        <html lang="en">
+          <body
+            className={`${inter.variable} antialiased`}
+          >
+            <Toaster />
+            {children}
+          </body>
+        </html>
+      </TRPCReactProvider>
+    </NuqsAdapter>
+  );
+}

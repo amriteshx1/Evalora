3f3c7a1d308e4c6d9332b54964a3ab977919d691
Amritesh Gautam <amriteshx18@gmail.com>
2026-05-05 19:26:02 +0530

    feat: implement premium subscription infrastructure, pricing UI, and access gating

---
+++ src/modules/premium/server/procedures.ts
@@ -0,0 +1,66 @@
+import { eq, count } from "drizzle-orm";
+
+import { db } from "@/db";
+import { interviewers, interviews } from "@/db/schema";
+
+import { polarClient } from "@/lib/polar";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+
+export const premiumRouter = createTRPCRouter({
+    getCurrentSubscription: protectedProcedure.query(async ({ ctx }) => {
+      const customer = await polarClient.customers.getStateExternal({
+        externalId: ctx.auth.user.id,
+      });
+
+      const subscription = customer.activeSubscriptions[0];
+
+      if(!subscription){
+        return null;
+      }
+
+      const product = await polarClient.products.get({
+        id: subscription.productId,
+      });
+
+      return product;
+    }),
+    getProducts: protectedProcedure.query(async () => {
+      const products = await polarClient.products.list({
+        isArchived: false,
+        isRecurring: true,
+        sorting: ["price_amount"],
+      });
+
+      return products.result.items;
+    }),
+    getFreeUsage: protectedProcedure.query(async ({ ctx }) => {
+        const customer = await polarClient.customers.getStateExternal({
+            externalId: ctx.auth.user.id,
+        });
+
+        const subscription = customer.activeSubscriptions[0];
+
+        if(subscription){
+            return null;
+        }
+
+        const [userMeetings] = await db 
+          .select({
+            count: count(interviews.id),
+          })
+          .from(interviews)
+          .where(eq(interviews.userId, ctx.auth.user.id));
+
+        const [userAgents] = await db 
+          .select({
+            count: count(interviewers.id),
+          })
+          .from(interviewers)
+          .where(eq(interviewers.userId, ctx.auth.user.id));
+
+        return {
+            meetingCount: userMeetings.count,
+            agentCount: userAgents.count,
+        };
+    })
+});

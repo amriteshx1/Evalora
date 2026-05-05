37789c7ab10c0632ba2fd570e04e9af46e6449cc
Amritesh Gautam <amriteshx18@gmail.com>
2026-05-05 19:19:18 +0530

    implement interviews tRPC and Stream call creation

---
+++ src/modules/interviews/server/procedures.ts
@@ -0,0 +1,332 @@
+import {z} from "zod";
+import { TRPCError } from "@trpc/server";
+import JSONL from "jsonl-parse-stringify";
+
+import { and, count, desc, eq, getTableColumns, ilike, inArray, sql } from "drizzle-orm";
+
+import { db } from "@/db";
+import { interviewers, interviews, user } from "@/db/schema";
+import { createTRPCRouter, premiumProcedure, protectedProcedure } from "@/trpc/init";
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
+import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
+import { MeetingStatus, StreamTranscriptItem } from "../types";
+import { streamVideo } from "@/lib/stream-video";
+import { generateAvatarUri } from "@/lib/avatar";
+import { streamChat } from "@/lib/stream-chat";
+
+export const interviewsRouter = createTRPCRouter({
+    generateChatToken: protectedProcedure.mutation(async ({ctx}) => {
+      const token = streamChat.createToken(ctx.auth.user.id);
+      await streamChat.upsertUser({
+        id: ctx.auth.user.id,
+        role: "admin",
+      });
+
+      return token;
+    }),
+    getTranscript: protectedProcedure
+      .input(z.object({ id: z.string() }))
+      .query(async ({ input, ctx }) => {
+        const [existingMeeting] = await db
+          .select()
+          .from(interviews)
+          .where(
+            and(eq(interviews.id, input.id), eq(interviews.userId, ctx.auth.user.id))
+          );
+
+        if(!existingMeeting){
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Interview not found",
+          });
+        }
+
+        if(!existingMeeting.transcriptUrl){
+          return [];
+        }
+
+        const transcript = await fetch(existingMeeting.transcriptUrl)
+          .then((res) => res.text())
+          .then((text) => JSONL.parse<StreamTranscriptItem>(text))
+          .catch(() => {
+            return [];
+          });
+
+        const speakerIds = [
+          ...new Set(transcript.map((item) => item.speaker_id)),
+        ];
+
+        const userSpeakers = await db
+         .select()
+         .from(user)
+         .where(inArray(user.id, speakerIds))
+         .then((users) =>
+           users.map((user) => ({
+             ...user,
+             image: 
+               user.image ??
+               generateAvatarUri({ seed: user.name, variant: "initials"}),
+           }))
+         );
+
+         const agentSpeakers = await db
+          .select()
+          .from(interviewers)
+          .where(inArray(interviewers.id, speakerIds))
+          .then((interviewers) =>
+            interviewers.map((interviewer) => ({
+              ...interviewer,
+              image: generateAvatarUri({
+                seed: interviewer.name,
+                variant: "botttsNeutral",
+              }),
+            }))
+          );
+
+        const speakers = [...userSpeakers, ...agentSpeakers];
+
+        const transcriptWithSpeakers = transcript.map((item) => {
+          const speaker = speakers.find(
+            (speaker) => speaker.id === item.speaker_id
+          );
+
+          if(!speaker){
+          return {
+            ...item,
+            user: {
+              name: "Unknown",
+              image: generateAvatarUri({
+                seed: "Unknown",
+                variant: "initials",
+              }),
+            },
+          };
+        }
+
+        return {
+          ...item,
+          user: {
+            name: speaker.name,
+            image: speaker.image,
+          },
+        };
+        })
+
+        return transcriptWithSpeakers;
+      }),
+    generateToken: protectedProcedure.mutation(async ({ ctx }) => {
+      await streamVideo.upsertUsers([
+        {
+          id: ctx.auth.user.id,
+          name: ctx.auth.user.name,
+          role: "admin",
+          image:
+            ctx.auth.user.image ??
+            generateAvatarUri({ seed: ctx.auth.user.name, variant: "initials"}),
+        },
+      ]);
+
+      const expirationTime = Math.floor(Date.now() / 1000) + 3600;
+      const issuedAt = Math.floor(Date.now() / 1000) - 60;
+
+      const token = streamVideo.generateUserToken({
+        user_id: ctx.auth.user.id,
+        exp: expirationTime,
+        validity_in_seconds: issuedAt,
+      });
+
+      return token;
+    }),
+    remove: protectedProcedure
+      .input(z.object({ id: z.string() }))
+      .mutation(async ({ctx, input}) => {
+        const [removedMeeting] = await db
+          .delete(interviews)
+          .where(
+            and(
+              eq(interviews.id, input.id),
+              eq(interviews.userId, ctx.auth.user.id),
+            )
+          )
+          .returning();
+
+        if(!removedMeeting){
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Interview not found",
+          });
+        }
+
+        return removedMeeting;
+    }),
+    update: protectedProcedure
+      .input(meetingsUpdateSchema)
+      .mutation(async ({ctx, input}) => {
+        const [updatedMeeting] = await db
+          .update(interviews)
+          .set(input)
+          .where(
+            and(
+              eq(interviews.id, input.id),
+              eq(interviews.userId, ctx.auth.user.id),
+            )
+          )
+          .returning();
+
+        if(!updatedMeeting){
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Interview not found",
+          });
+        }
+
+        return updatedMeeting;
+    }),
+    create: premiumProcedure("meetings")
+      .input(meetingsInsertSchema)
+      .mutation(async ({ input, ctx }) => {
+        const [createdMeeting] = await db
+          .insert(interviews)
+          .values({
+            ...input,
+            userId: ctx.auth.user.id,
+          })
+          .returning();
+
+          const call = streamVideo.video.call("default", createdMeeting.id);
+          await call.create({
+            data: {
+              created_by_id: ctx.auth.user.id,
+              custom: {
+                interviewId: createdMeeting.id,
+                interviewName: createdMeeting.name
+              },
+              settings_override: {
+                transcription: {
+                  language: "en",
+                  mode: "auto-on",
+                  closed_caption_mode: "auto-on",
+                },
+                recording: {
+                  mode: "auto-on",
+                  quality: "1080p",
+                },
+              },
+            },
+          });
+
+          const [existingAgent] = await db
+              .select()
+              .from(interviewers)
+              .where(eq(interviewers.id, createdMeeting.interviewerId));
+
+          if(!existingAgent){
+            throw new TRPCError({
+              code: "NOT_FOUND",
+              message: "Interviewer not found",
+            });
+          }
+
+          await streamVideo.upsertUsers([
+            {
+              id: existingAgent.id,
+              name: existingAgent.name,
+              role: "user",
+              image: generateAvatarUri({
+                seed: existingAgent.name,
+                variant: "botttsNeutral",
+              }),
+            },
+          ]);
+
+          return createdMeeting;
+      }),
+    getOne: protectedProcedure
+      .input(z.object({ id: z.string() }))
+      .query( async ({ input, ctx }) => {
+        const [existingMeeting] = await db
+          .select({
+            ...getTableColumns(interviews),
+            interviewer: interviewers,
+            duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
+          })
+          .from(interviews)
+          .innerJoin(interviewers, eq(interviews.interviewerId, interviewers.id))
+          .where(
+            and(
+              eq(interviews.id, input.id),
+              eq(interviews.userId, ctx.auth.user.id),
+            )
+          )
+
+          if(!existingMeeting){
+            throw new TRPCError({code: "NOT_FOUND", message: "Interview Not Found"});
+          }
+
+        return existingMeeting;
+    }),
+    getMany: protectedProcedure
+    .input(z.object({
+      page: z.number().default(DEFAULT_PAGE),
+      pageSize: z
+       .number()
+       .min(MIN_PAGE_SIZE)
+       .max(MAX_PAGE_SIZE)
+       .default(DEFAULT_PAGE_SIZE),
+      search: z.string().nullish(),
+      interviewerId: z.string().nullish(),
+      status: z
+        .enum([
+          MeetingStatus.Upcoming,
+          MeetingStatus.Active,
+          MeetingStatus.Completed,
+          MeetingStatus.Processing,
+          MeetingStatus.Cancelled,
+        ])
+        .nullish(),
+    }))
+    .query( async ({ ctx, input }) => {
+        const { search, page, pageSize, status, interviewerId } = input;
+
+        const data = await db
+          .select({
+            ...getTableColumns(interviews),
+            interviewer: interviewers,
+            duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
+          })
+          .from(interviews)
+          .innerJoin(interviewers, eq(interviews.interviewerId, interviewers.id))
+          .where(
+            and(
+              eq(interviews.userId, ctx.auth.user.id),
+              search ? ilike(interviews.name, `%${search}%`) : undefined,
+              status ? eq(interviews.status, status) : undefined,
+              interviewerId ? eq(interviews.interviewerId, interviewerId) : undefined,
+            )
+          )
+          .orderBy(desc(interviews.createdAt), desc(interviews.id))
+          .limit(pageSize)
+          .offset((page - 1) * pageSize)
+        
+        const [total] = await db
+          .select({ count: count() })
+          .from(interviews)
+          .innerJoin(interviewers, eq(interviews.interviewerId, interviewers.id))
+          .where(
+            and(
+              eq(interviews.userId, ctx.auth.user.id),
+              search ? ilike(interviews.name, `%${search}%`) : undefined,
+              status ? eq(interviews.status, status) : undefined,
+              interviewerId ? eq(interviews.interviewerId, interviewerId) : undefined,
+            )
+          );
+
+        const totalPages = Math.ceil(total.count / pageSize);
+
+        return {
+          items: data,
+          total: total.count,
+          totalPages,
+        };
+    }),
+});

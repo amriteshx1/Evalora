f5702c9120970ff08cd7629a3266d543fd94ee90
Amritesh Gautam <amriteshx18@gmail.com>
2026-06-16 18:48:08 +0530

    feat: add scoreboard router to appRouter

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,12 +1,16 @@
+import { candidatesRouter } from "@/modules/candidates/server/procedures";
 import { interviewersRouter } from "@/modules/interviewers/server/procedures";
 import { interviewsRouter } from "@/modules/interviews/server/procedures";
 import { premiumRouter } from "@/modules/premium/server/procedures";
+import { scoreboardRouter } from "@/modules/scoreboard/server/procedures";
 import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
+  candidates: candidatesRouter,
   interviewers: interviewersRouter,
   interviews: interviewsRouter,
   premium: premiumRouter,
+  scoreboard: scoreboardRouter,
 });
 
 export type AppRouter = typeof appRouter;
